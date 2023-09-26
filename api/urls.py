42491f36aac06f3a703f92545618e81fd3368b4a
hussein-salemm <husseinsalem177@gmail.com>
2023-09-26 22:10:02 +0300

    Token Done

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import MealViewSet, RatingViewSet
+from .views import MealViewSet, RatingViewSet, UserViewSet
 
 router = routers.DefaultRouter()
 router.register("meals", MealViewSet)
 router.register("ratings", RatingViewSet)
-
+router.register("users", UserViewSet, basename="users")
 urlpatterns = [
     path("", include(router.urls)),
 ]
